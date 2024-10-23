e74ca6f9f4f087c50fa4df8e7fa13669ce72a276
Pipapupu0-0 <vika.margaryan.06@list.ru>
2024-10-24 01:28:02 +0300

    weather api

--- components/screens/Home_screen.js
+++ components/screens/Home_screen.js
@@ -1,10 +1,29 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch} from 'react-redux';
 import { changeName } from '../reducers/action';
-import {Button, View, TextInput, StyleSheet} from 'react-native';
+import {Button, View, TextInput, StyleSheet, FlatList, Text} from 'react-native';
 
 
 export default function HomeScreen({navigation}) {
+    const [weather, setWeather] = useState([])
+    useEffect(() => {
+        getWeather();
+    }, []);
+
+    const getWeather = () => {
+        const URL = "https://www.weatherapi.com/docs/weather_conditions.json";
+
+        fetch(URL)
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setWeather(data);
+                console.log(data);
+            });
+    };
+    
+
     const [name, setName] = useState("");
     const dispatch = useDispatch();
 
@@ -15,6 +34,11 @@ export default function HomeScreen({navigation}) {
                 dispatch(changeName(name));
                 navigation.navigate('Профиль');
             }}/>
+            <FlatList data={weather} renderItem={({ item }) => (
+                <View>
+                    <Text style={styles.text}>Day: {item.day} | Night: {item.night}</Text>
+                </View>
+            )}/>
         </View>
     );
 }
@@ -27,5 +51,10 @@ const styles = StyleSheet.create({
         marginVertical: 30,
         marginHorizontal: '10%',
         width: '80%',
+    },
+    text: {
+        textAlign: 'center',
+        marginVertical: 30,
+        fontSize: 18,
     },
 });
